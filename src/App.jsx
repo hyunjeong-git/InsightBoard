6de4932264b6719e18ed7daea589f7136ab6fdb8
hjryu-lab021 <hjryu@lab021.co.kr>
2025-06-17 23:15:08 +0900

    feat: 대시보드 페이지, Card 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import {
 } from "react-router-dom";
 import Login from './pages/Login.jsx';
 import SingUp from './pages/Signup.jsx';
+import Dashboard from './features/dashboard/DashboardPage.jsx';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />}/>
+      <Route path="/" element={<Dashboard />}/>
       <Route path="/login" element={<Login />}/>
       <Route path="/signup" element={<SingUp />}/>
     </Routes>
---
+++ src/features/dashboard/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import SummaryCards from './SummaryCards';
+
+function DashboardPage() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+        <SummaryCards></SummaryCards>
+    </div>
+  );
+}
+
+export default DashboardPage;
