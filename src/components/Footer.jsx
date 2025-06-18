7bb887b3457e482db3518b5553211c1d06285d37
hjryu-lab021 <hjryu@lab021.co.kr>
2025-06-19 00:42:09 +0900

    chore: 파비콘과 페이지 제목 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} My Website. All rights reserved.
+          &copy; {new Date().getFullYear()} My Website. All rights reserved. Icons by Icons8
         </p>
       </div>
     </footer>
