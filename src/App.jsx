ed3120c47c135914d796a75f34198eeb9be8620c
hjryu-lab021 <hjryu@lab021.co.kr>
2025-06-18 16:51:19 +0900

    feat: 페이지 레이아웃을 위한 Header/Footer 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-import Login from './pages/Login.jsx';
-import SingUp from './pages/Signup.jsx';
-import Dashboard from './features/dashboard/DashboardPage.jsx';
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import SingUp from "./pages/Signup.jsx";
+import Dashboard from "./features/dashboard/DashboardPage.jsx";
+import Layout from "./layout/Layout.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<SingUp />}/>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SingUp />} />
+      </Route>
     </Routes>
   );
 }
 
-export default App
+export default App;
