5703b50d358ed920d414438c51c885029d48d0fe
hjryu-lab021 <hjryu@lab021.co.kr>
2025-06-24 23:31:21 +0900

    feat: 메인 페이지로 이동하는 링크 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 shadow-m bg-gray-800 text-white p-4">
-      <h1 className="text-2xl font-bold">InsightBoard</h1>
+      <Link to="/">
+        <h1 className="text-2xl font-bold">InsightBoard</h1>
+      </Link>
     </header>
   );
 }
