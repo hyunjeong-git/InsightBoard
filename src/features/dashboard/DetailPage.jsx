ff8f8534d795bcd5d5b882f118e68c248ac3363c
hjryu-lab021 <hjryu@lab021.co.kr>
2025-07-01 23:30:49 +0900

    style: React 문법에 맞게 SVG 속성 네이밍 변경

--- src/features/dashboard/DetailPage.jsx
+++ src/features/dashboard/DetailPage.jsx
@@ -25,12 +25,12 @@ const DetailPage = () => {
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          class="h-6 w-6"
+          className="h-6 w-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M7 16l-4-4m0 0l4-4m-4 4h18"
           ></path>
         </svg>
