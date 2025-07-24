97485b8fd170f0592230fdb68b6ccdd9ceb00b7f
HJ <wjdgusfb11@gmail.com>
2025-07-24 23:58:23 +0900

    feat: Create Board 모달 내부 내용 구현

--- src/features/dashboard/AddNewBoardCardPopover.jsx
+++ src/features/dashboard/AddNewBoardCardPopover.jsx
@@ -1,17 +1,13 @@
 import React, { useState, useRef, useEffect } from "react";
-
+import VisibilityDropdown from "./VisibilityDropdown";
 
 function AddNewBoardCardPopover() {
-  console.log('test')
   const [isOpen, setIsOpen] = useState(false);
   const popoverRef = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (
-        popoverRef.current &&
-        !popoverRef.current.contains(event.target)
-      ) {
+      if (popoverRef.current && !popoverRef.current.contains(event.target)) {
         setIsOpen(false);
       }
     };
@@ -21,21 +17,63 @@ function AddNewBoardCardPopover() {
 
   return (
     <div className="relative inline-block" ref={popoverRef}>
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="text-gray"
-      >
+      <button onClick={() => setIsOpen(!isOpen)} className="text-gray">
         Create new board
       </button>
 
       {isOpen && (
-        <div className="absolute left-0 mt-2 w-48 rounded-lg shadow-lg bg-white border border-gray-200 p-4 z-10">
-          <p className="text-sm text-gray-800">Popover</p>
+        <div className="absolute flex flex-col w-[304px] h-[602px] rounded-lg shadow-lg bg-white border border-gray-200 p-4">
+          <p className="font-medium text-gray-600 mb-2">Create board</p>
+          <div
+            style={{
+              backgroundImage: 'url("/public/images/background_img_1.svg")',
+            }}
+            className="w-[200px] h-[120px]"
+          >
+            <img src="/public/images/board_img.svg"></img>
+          </div>
+          <form>
+            <div className="text-[12px] leading-[16px] font-bold text-gray-700 text-left">
+              Background
+            </div>
+
+            <div className="board-title-wrap mb-4">
+              <div className="text-[12px] leading-[16px] font-bold text-gray-700 text-left">
+                board title
+                <span className="text-red-500">*</span>
+              </div>
+              <input
+                type="text"
+                className="w-full p-1 border border-gray-200 rounded mt-1"
+                required
+              />
+              <div className="mt-1 text-left">
+                <p className="text-[14px] leading-[16px] font-normal">
+                  Board title is required
+                </p>
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <div className="text-[12px] leading-[16px] font-bold text-gray-700 text-left mb-1">
+                Visibility
+              </div>
+              <VisibilityDropdown />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <button class="bg-gray-100 hover:bg-gray-200 text-gray-800 text-[14px] font-medium py-1 px-4 rounded justify-center">
+                Create
+              </button>
+              <button class="bg-gray-100 hover:bg-gray-200 text-gray-800 text-[14px] font-medium py-1 px-4 rounded justify-center">
+                <span>Start with a template</span>
+              </button>
+            </div>
+          </form>
         </div>
       )}
     </div>
   );
 }
-
 
 export default AddNewBoardCardPopover;
