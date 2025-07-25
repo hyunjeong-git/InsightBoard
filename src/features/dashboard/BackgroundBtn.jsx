1c959b9fa9218f4f637a0feec4839fb08a267b61
HJ <wjdgusfb11@gmail.com>
2025-07-26 00:09:39 +0900

    Feat: check icon 추가

--- src/features/dashboard/BackgroundBtn.jsx
+++ src/features/dashboard/BackgroundBtn.jsx
@@ -1,11 +1,27 @@
 function BackgroundBtn({ onClick, isActive, url }) {
   return (
     <button
-      className="w-[64px] h-[32px] bg-no-repeat bg-cover mb-1"
+      className="flex justify-center items-center w-[64px] h-[32px] bg-no-repeat bg-cover mb-1"
       style={{
         backgroundImage: `url("/public/images/${url}")`,
       }}
-    ></button>
+    >
+      <svg /* check icon */
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-5"
+        style={{ visibility: isActive ? "visible" : "hidden" }}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m4.5 12.75 6 6 9-13.5"
+        />
+      </svg>
+    </button>
   );
 }
 
