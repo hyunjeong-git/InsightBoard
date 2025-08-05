a4a1610bc52a8eec106650317f63a67de5fc1aad
HJ <wjdgusfb11@gmail.com>
2025-08-05 23:18:08 +0900

    feat: Card 리스트 컴포넌트 생성 및 Tailwind 스타일 적용

---
+++ src/components/dashboard/CardList.jsx
@@ -0,0 +1,20 @@
+const CardList = ({title}) => {
+  return (
+    <div className="card-list flex flex-col justify-between w-[272] max-h-full bg-[#f1f2f4] rounded-xl p-4">
+      <div className="card-list-header flex justify-between items-center mb-4">
+        <div>
+          <span className="text-sm/[20px] text-[#172B4D] font-semibold">{title}</span>
+        </div>
+        <div className="card-list-header-buttons">
+          <button className="btn btn-primary">...</button>
+        </div>
+      </div>
+      <div className="card-list-content">{/* card 컴포넌트 */}</div>
+      <div className="card-list-footer">
+        <button className="btn w-full pt-[6px] pr-3 pb-[6px] pl-2 rounded-lg text-left text-sm/[20px] text-[#44546F] font-medium hover:bg-[#091e4224] hover:text-[#172b4d]">+ Add a card</button>
+      </div>
+    </div>
+  );
+}
+
+export default CardList;
